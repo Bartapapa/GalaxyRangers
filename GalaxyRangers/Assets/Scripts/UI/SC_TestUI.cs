3ecca052439b36757cd2b48e3f410cacc513ac6c
GaeOklos <league25300@gmail.com>
2024-02-15 15:58:47 +0100

    Recup tout

--- GalaxyRangers/Assets/Scripts/UI/SC_TestUI.cs
+++ GalaxyRangers/Assets/Scripts/UI/SC_TestUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,32 +6,50 @@ using UnityEngine.UI;
 
 public class SC_TestUI : MonoBehaviour
 {
+    public CharacterHealth ch_health;
+
     public Slider healthSlider;
     public Slider easeHealthSlider;
-    public float maxHealth = 100f;
-    public float health;
+    // public float maxHealth = 100f;
+    // public float health;
     private float lerpSpeed = 0.075f;
 
-    private void Start()
+    private void OnEnable()
     {
-        health = maxHealth;
+        //ch_health.CharacterHurt -= OnCharacterHurt;
+        //ch_health.CharacterHurt += OnCharacterHurt;
     }
+
+    private void OnDisable()
+    {
+        //ch_health.CharacterHurt -= OnCharacterHurt;
+    }
+
+    private void OnCharacterHurt(CharacterHealth characterHealth)
+    {
+        //ch_health.Health.CurrentValue;
+        //ch_health.Health.MaxValue;
+    }
+
 
     private void Update()
     {
-        if (healthSlider.value != health) {
-            healthSlider.value = health;
+        if (healthSlider.value != ch_health.Health.CurrentValue) {
+            healthSlider.value = ch_health.Health.CurrentValue;
         }
         if (Input.GetKeyDown(KeyCode.Space)) {
             TakeDamage(10);
         }
-        if (healthSlider.value != easeHealthSlider.value) {
-            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, health, lerpSpeed);
+        if (ch_health.Health.CurrentValue != easeHealthSlider.value) {
+            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, ch_health.Health.CurrentValue, lerpSpeed);
         }
+        healthSlider.maxValue = ch_health.Health.MaxValue;
+        easeHealthSlider.maxValue = ch_health.Health.MaxValue;
+        // Camera.main;
     }
 
     private void TakeDamage(float damage)
     {
-        health -= damage;
+        // ch_health.Health.CurrentValue -= damage;
     }
 }
