ba12c2723dba09ccd1fe939b45e26386b3f8e2a9
Barthélémi Rapeaux <rapeauxb@gmail.com>
2024-02-20 12:20:21 +0100

    FIX: Procedural generation

--- GalaxyRangers/Assets/Scripts/Room/InteractibleShopRunStatut.cs
+++ GalaxyRangers/Assets/Scripts/Room/InteractibleShopRunStatut.cs
@@ -38,29 +38,29 @@ public class InteractibleShopRunStatut : Interactible
 
     private void OnEnable()
     {
-        switch (_statutType)
-        {
-            case StatutType.Health:
-                _textValue.text = _ValueByLevel_Health[_currentLevel].ToString();
-                _textPrice.text = _ValueByLevel_Health[_currentLevel].ToString();
-                price = _PriceByLevel_Health[_currentLevel];
-                break;
-            case StatutType.Damage:
-                _textValue.text = _ValueByLevel_Damage[_currentLevel].ToString();
-                _textPrice.text = _ValueByLevel_Damage[_currentLevel].ToString();
-                price = _PriceByLevel_Damage[_currentLevel];
-                break;
-            case StatutType.DashDistance:
-                _textValue.text = _ValueByLevel_DashDistance[_currentLevel].ToString();
-                _textPrice.text = _ValueByLevel_DashDistance[_currentLevel].ToString();
-                price = _PriceByLevel_DashDistance[_currentLevel];
-                break;
-            case StatutType.Speed:
-                _textValue.text = _ValueByLevel_Speed[_currentLevel].ToString();
-                _textPrice.text = _ValueByLevel_Speed[_currentLevel].ToString();
-                price = _PriceByLevel_Speed[_currentLevel];
-                break;
-        }
+        //switch (_statutType)
+        //{
+        //    case StatutType.Health:
+        //        _textValue.text = _ValueByLevel_Health[_currentLevel].ToString();
+        //        _textPrice.text = _ValueByLevel_Health[_currentLevel].ToString();
+        //        price = _PriceByLevel_Health[_currentLevel];
+        //        break;
+        //    case StatutType.Damage:
+        //        _textValue.text = _ValueByLevel_Damage[_currentLevel].ToString();
+        //        _textPrice.text = _ValueByLevel_Damage[_currentLevel].ToString();
+        //        price = _PriceByLevel_Damage[_currentLevel];
+        //        break;
+        //    case StatutType.DashDistance:
+        //        _textValue.text = _ValueByLevel_DashDistance[_currentLevel].ToString();
+        //        _textPrice.text = _ValueByLevel_DashDistance[_currentLevel].ToString();
+        //        price = _PriceByLevel_DashDistance[_currentLevel];
+        //        break;
+        //    case StatutType.Speed:
+        //        _textValue.text = _ValueByLevel_Speed[_currentLevel].ToString();
+        //        _textPrice.text = _ValueByLevel_Speed[_currentLevel].ToString();
+        //        price = _PriceByLevel_Speed[_currentLevel];
+        //        break;
+        //}
     }
 
     private void TryToBuyItem()
