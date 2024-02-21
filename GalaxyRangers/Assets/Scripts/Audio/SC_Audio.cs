f3f99f28a2fc4233f26d299b7cd7ddfe6b81a836
GaeOklos <league25300@gmail.com>
2024-02-21 12:18:25 +0100

    Audio manager done

---
+++ GalaxyRangers/Assets/Scripts/Audio/SC_Audio.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Audio : MonoBehaviour
+{
+    /*To play the sound :
+    [SerializeField]
+    private AudioClip sound;
+
+    AudioManager.Instance.MyFunction();
+    */
+}
