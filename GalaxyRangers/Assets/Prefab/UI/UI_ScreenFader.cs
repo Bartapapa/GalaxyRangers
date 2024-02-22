d420c6b4f4f3a9f799d40d49fb5a8414dc4bfc42
Barthélémi Rapeaux <rapeauxb@gmail.com>
2024-02-22 11:32:37 +0100

    ADD: Fade FIX: Combat

---
+++ GalaxyRangers/Assets/Prefab/UI/UI_ScreenFader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ScreenFader : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject screen;
+
+    public void FadeInSide()
+    {
+        animator.Play("FadeInSide", 0, 0);
+    }
+
+    public void FadeOutSide()
+    {
+        animator.Play("FadeOutSide", 0, 0);
+    }
+
+    public void FadeInScreen()
+    {
+        animator.Play("FadeInScreen", 0, 0);
+    }
+
+    public void FadeOutScreen()
+    {
+        animator.Play("FadeOutScreen", 0, 0);
+    }
+
+    public void ToggleVisibility(bool see)
+    {
+        screen.SetActive(see);
+    }
+}
